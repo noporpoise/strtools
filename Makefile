7518507485aa4d7fea1fd982e57d92156c4b512d
Isaac Turner <noporpoise@users.sourceforge.net>
2012-06-13 17:23:27 +0100

    Added commands uc and lc for changing case

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 all:
 	gcc -o strlen -Wall strlen.c
+	gcc -o uc -Wall change_case.c
+	gcc -o lc -Wall -DLOWER_CASE=1 change_case.c
 
 clean:
-	rm strlen
+	rm strlen uc lc
--- change_case.c
+++ change_case.c
@@ -1,11 +1,11 @@
 /*
- strlen.c
- project: strlen
- url: http://sourceforge.net/projects/strlen/
+ change_case.c
+ project: change string case
  author: Isaac Turner <turner.isaac@gmail.com>
  Copyright (C) 27-Jan-2012
 
- build: gcc -o strlen -Wall strlen.c
+ build: gcc -o uc -Wall uc.c
+ (or just type 'make')
 
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
@@ -24,6 +24,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
+#ifdef LOWER_CASE
+  #define change_case(x) tolower(x)
+  #define CMDLINE "lc"
+#else
+  #define change_case(x) toupper(x)
+  #define CMDLINE "uc"
+#endif
 
 int main(int argc, char* argv[])
 {
@@ -31,10 +40,9 @@ int main(int argc, char* argv[])
      (strcasecmp(argv[1], "-h") == 0 || strcasecmp(argv[1], "--h") == 0 ||
       strcasecmp(argv[1], "-help") == 0 || strcasecmp(argv[1], "--help") == 0))
   {
-    printf("usage: strlen [string1 ..]\n");
+    printf("usage: "CMDLINE" [string1 ..]\n");
     printf(
-"  Prints length of strings, one per line.  If args not given reads from STDIN\n"
-"  and prints the length of each line.\n");
+"  Print string with case changed.  If args not given reads from STDIN.\n");
     exit(EXIT_FAILURE);
   }
 
@@ -44,43 +52,24 @@ int main(int argc, char* argv[])
   if(argc == 1)
   {
     // Read from STDIN
-    int old_c = EOF, c = getchar();
-    unsigned long length = 0;
+    int c;
 
-    while(c != EOF)
+    while((c = getchar()) != EOF)
     {
-      if(c == '\n' || c == '\r')
-      {
-        if(old_c == '\r' && c == '\n')
-        {
-          // Character read in was part of previous end-of-line
-        }
-        else
-        {
-          printf("%lu\n", length);
-          length = 0;
-        }
-      }
-      else
-      {
-        length++;
-      }
-
-      old_c = c;
-      c = getchar();
-    }
-
-    if(old_c != '\n' && old_c != '\r')
-    {
-      printf("%lu\n", length);
+      printf("%c", change_case(c));
     }
   }
   else
   {
-    int i;
+    int i, j;
     for(i = 1; i < argc; i++)
     {
-      printf("%lu\n", (unsigned long)strlen(argv[i]));
+      for(j = 0; argv[i][j] != '\0'; j++)
+      {
+        printf("%c", change_case((int)argv[i][j]));
+      }
+
+      printf("\n");
     }
   }
 
